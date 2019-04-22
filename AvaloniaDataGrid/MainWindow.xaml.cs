76136da6d744339afd2b1d974cc9081060d4b664
Gary Hertel <garyhertel@gmail.com>
2019-04-21 20:50:45 -0700

    Adding sample code

---
+++ AvaloniaDataGrid/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using Avalonia;
+using Avalonia.Collections;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using System.Collections.Generic;
+
+namespace AvaloniaDataGrid
+{
+	public class MainWindow : Window
+	{
+		private List<TestItem> testItems;
+		private DataGridCollectionView collectionView;
+
+		private Grid grid;
+		private DataGrid dataGrid;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+#if DEBUG
+			this.AttachDevTools();
+#endif
+		}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+			grid = this.FindControl<Grid>("Grid");
+
+			testItems = new List<TestItem>();
+			for (int i = 0; i < 2; i++)
+				testItems.Add(new TestItem());
+
+			collectionView = new DataGridCollectionView(testItems);
+
+			dataGrid = new DataGrid()
+			{
+				//HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Stretch,
+				//VerticalAlignment = Avalonia.Layout.VerticalAlignment.Stretch,
+				Background = new SolidColorBrush(Colors.White),
+				RowBackground = new SolidColorBrush(Colors.White),
+				AlternatingRowBackground = new SolidColorBrush(Colors.White),
+				Items = collectionView,
+				AutoGenerateColumns = true,
+			};
+
+			//TestsStackPanel(); // resizes correctly
+			TestGrid(); // doesn't resize correctly
+
+			this.Tapped += MainWindow_Tapped;
+		}
+
+		private void TestGrid()
+		{
+			grid.Children.Add(dataGrid);
+		}
+
+		private void TestsStackPanel()
+		{
+			StackPanel stackPanel = new StackPanel()
+			{
+				Orientation = Orientation.Vertical,
+				Background = new SolidColorBrush(Colors.Orange),
+			};
+			stackPanel.Children.Add(dataGrid);
+			Content = stackPanel;
+		}
+
+		private void MainWindow_Tapped(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+		{
+			for (int i = 0; i < 1; i++)
+				testItems.Add(new TestItem());
+
+			collectionView.Refresh(); 
+
+			/*dataGrid.Measure(Bounds.Size);
+			Size desiredSize = grid.DesiredSize;
+			Size dataGridDesiredSize = dataGrid.DesiredSize;
+			grid.InvalidateMeasure();
+			grid.InvalidateArrange();
+			dataGrid.InvalidateMeasure();
+			dataGrid.InvalidateArrange();
+			InvalidateMeasure();
+			InvalidateArrange();*/
+		}
+	}
+}
---
+++ AvaloniaDataGrid/TestItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AvaloniaDataGrid
+{
+	public class TestItem
+	{
+		public int Index { get; set; } = 0;
+
+		public int Num0 { get; set; } = 0;
+		public int Num1 { get; set; } = 1;
+		public int Num2 { get; set; } = 2;
+
+		public override string ToString()
+		{
+			return Index.ToString();
+		}
+	}
+}
